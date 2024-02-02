90e1b30de18465f4a0e3815ff41446ec4becbf0f
Tony desktop <tonysanj13@gmail.com>
2024-02-02 16:13:11 +0100

    fix: test lib index

--- libs/test/src/index.ts
+++ libs/test/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/test/test.component';
+export * from './lib/uiStateMock/uiStateMock.mock'
+export * from './lib/userStateMock/userStateMock.mock'
+
+export * from './lib/test.module'
