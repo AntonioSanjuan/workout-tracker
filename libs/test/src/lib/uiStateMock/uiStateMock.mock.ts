11642f7377c1cb8ebed97a1645a002eeca0553b9
Tony desktop <tonysanj13@gmail.com>
2024-02-02 15:31:17 +0100

    feature: added test lib

---
+++ libs/test/src/lib/uiStateMock/uiStateMock.mock.ts
@@ -0,0 +1,5 @@
+import { UI_FEATURE_KEY, uiInitialState } from "@workout-tracker/shared-store";
+
+export const uiStateMock = {
+    [UI_FEATURE_KEY]: uiInitialState
+}
---
+++ libs/test/src/lib/userStateMock/userStateMock.mock.ts
@@ -0,0 +1,5 @@
+import { USER_FEATURE_KEY, userInitialState } from "@workout-tracker/shared-store";
+
+export const userStateMock = {
+    [USER_FEATURE_KEY]: userInitialState
+}
