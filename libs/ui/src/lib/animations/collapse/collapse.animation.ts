a8f2e979b119331045cbe882a5ebeb9f7804db4d
Tony desktop <tonysanj13@gmail.com>
2024-02-03 23:19:26 +0100

    feature: collapse animation fixed

--- libs/ui/src/lib/animations/collapse/collapse.animation.ts
+++ libs/ui/src/lib/animations/collapse/collapse.animation.ts
@@ -10,8 +10,8 @@ import {
   const DEFAULT_DURATION = 500;
 
   export const collapseAnimation = trigger('collapse', [
-    state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
-    state('true', style({ height: '0', visibility: 'hidden' })),
+    state('false', style({ left: AUTO_STYLE })),
+    state('true', style({ left: '-100%' })),
     transition('false => true', animate(`${DEFAULT_DURATION}ms ease-in`)),
     transition('true => false', animate(`${DEFAULT_DURATION}ms ease-out`))
   ])
