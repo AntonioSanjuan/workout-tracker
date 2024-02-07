6d0f1bbbb9bea373a0568b7e24a6810e20f81a48
Tony desktop <tonysanj13@gmail.com>
2024-02-07 15:46:27 +0100

    feature: added userEffects

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { reducers, rootInitialState } from './store/reducers';
-import { LayoutEffects, RequestEffects, UiEffects } from '@workout-tracker/shared-store';
+import { LayoutEffects, RequestEffects, UiEffects, UserEffects } from '@workout-tracker/shared-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { CoreModule } from './core/core.module';
@@ -41,7 +41,7 @@ import { LibsServicesApiModule } from '@workout-tracker/services/api';
         },
       }
     ),
-    EffectsModule.forRoot([UiEffects, RequestEffects, LayoutEffects]),
+    EffectsModule.forRoot([UiEffects, RequestEffects, LayoutEffects, UserEffects]),
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({ logOnly: !isDevMode() }),
   ],
