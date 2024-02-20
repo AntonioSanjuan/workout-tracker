24440faa9b99530cf8f3484c484bcce5f5c27aad
Tony desktop <tonysanj13@gmail.com>
2024-02-20 20:59:19 +0100

    feature: linked service with model

--- libs/services/exercises/src/lib/exercises.service.ts
+++ libs/services/exercises/src/lib/exercises.service.ts
@@ -1,26 +1,26 @@
 import { Injectable, inject } from "@angular/core";
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore'
-import { UserSettings } from "@workout-tracker/models";
+import { Exercise } from "@workout-tracker/models";
 import firebase from 'firebase/compat/app/';
 import { Observable, of } from "rxjs";
 @Injectable()
 export class ExercisesService {
     private firebaseDataBase: AngularFirestore = inject(AngularFirestore)
 
-    private getExercisesDocRef(userId: string): AngularFirestoreDocument<UserSettings> {
+    private getExercisesDocRef(userId: string): AngularFirestoreDocument<Exercise> {
         return this.firebaseDataBase.collection('user').doc(userId)
     }
 
-    public getExercises(userId: string|undefined): Observable<any> {
-        return of({})
+    public getExercises(userId: string|undefined): Observable<Exercise> {
+        return of({} as Exercise)
     }
 
-    public setExercises(userId: string|undefined, exercise: any): Observable<any> {
-        return of({})  
+    public setExercises(userId: string|undefined, exercise: Exercise): Observable<Exercise> {
+        return of({} as Exercise)  
     }
 
-    public updateExercises(userId: string|undefined, exercise: any): Observable<any> {
-        return of({})
+    public updateExercises(userId: string|undefined, exercise: Exercise): Observable<Exercise> {
+        return of({} as Exercise)
     }
 
     public deleteExercise(userId: string|undefined): Observable<boolean> {
