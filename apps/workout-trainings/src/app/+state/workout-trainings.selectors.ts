bdbdb77978320a6c931530134fb74895081a5ca9
Tony desktop <tonysanj13@gmail.com>
2024-04-10 09:29:23 +0200

    feature: added training/id view

--- apps/workout-trainings/src/app/+state/workout-trainings.selectors.ts
+++ apps/workout-trainings/src/app/+state/workout-trainings.selectors.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector } from '@ngrx/store';
 import { WorkoutTrainingsState } from './models/workoutTrainingsState.model';
-import { WORKOUT_TRAININGS_FEATURE_KEY } from './workout-trainings.reducer';
+import { WORKOUT_TRAININGS_FEATURE_KEY } from '@workout-tracker/shared-store';
 
 export const selectWorkoutTrainingsState =
   createFeatureSelector<WorkoutTrainingsState>(WORKOUT_TRAININGS_FEATURE_KEY);
